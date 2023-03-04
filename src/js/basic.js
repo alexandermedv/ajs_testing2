38cb9aed8d3bbebec4484ec89db2553d8be49892
alexandermedv <alexandermed@mail.ru>
2023-03-05 01:54:58 +0300

    add appveyor

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,7 @@
 export default function health(person) {
   let result = '';
 
-  if (parseInt(person['health']) > 50) {
+  if (parseInt(person["health"]) > 50) {
     result = 'healthy';
   } else if (parseInt(person['health']) < 15) {
     result = 'critical';
